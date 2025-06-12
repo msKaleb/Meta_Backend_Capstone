120d036638a6d267a00c98a492eaf51c5b2f6a8b
msKaleb <sjmikel@gmx.es>
2025-06-12 07:14:34 +0200

    unique constraint in models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -8,12 +8,15 @@ class Booking(models.Model):
   No_of_guests = models.SmallIntegerField()
   BookingDate = models.DateTimeField()
 
+  class Meta:
+    unique_together = ('Name', 'BookingDate')
+
   def __str__(self):
     return f'{self.Name} - {self.No_of_guests} guest(s)'
 
 
 class Menu(models.Model):
-  Title = models.CharField(max_length=255)
+  Title = models.CharField(max_length=255, unique=True)
   Price = models.DecimalField(max_digits=10, decimal_places=2)
   Inventory = models.SmallIntegerField()
 
